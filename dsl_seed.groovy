56201cd504e8dd561e65a0b9d289779667a4ba46
Sergey Harbach <sgoser@gmail.com>
2023-01-28 15:51:28 +0000

    Changes in groovy script

--- dsl_seed.groovy
+++ dsl_seed.groovy
@@ -1,18 +1,22 @@
-job("dsl-seed"){
-   scm {
+folder('SeedJobsFolder') {
+    displayName('SeedJobsFolder')
+    description('Folder for Seed Jobs')
+}
+
+job("SeedJobsFolder/dsl_seed_job"){
+    scm {
         github('sgoser/jenkins.casc', 'main')
     }
-            steps {
-              shell('''
-                ls -l
-                pwd
-                rm -rf /var/lib/jenkins/jobs/*0.*
-                pwd
-                ls -l /var/lib/jenkins/jobs/
-                mv /var/lib/jenkins/workspace/Hometask20/jobs/* '/var/lib/jenkins/jobs/'
-                ls -l /var/lib/jenkins/jobs/
-''')
-     
-        }
-
+    steps {
+        shell("""
+            ls -l /var/lib/jenkins/jobs/
+            ls -l /var/lib/jenkins/jobs/SeedJobsFolder/
+            ls -l /var/lib/jenkins/jobs/SeedJobsFolder/jobs/
+            rm -rf /var/lib/jenkins/jobs/SeedJobsFolder/jobs/0*
+            pwd
+            ls -l
+            mv /var/lib/jenkins/workspace/SeedJobsFolder/dsl_seed_job/jobs/* /var/lib/jenkins/jobs/SeedJobsFolder/jobs/
+            ls -l /var/lib/jenkins/jobs/SeedJobsFolder/jobs/
+        """)
     }
+}
